6f0ab4be26f9e5e260b4946a35bb3987977f134f
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-05-06 06:33:12 +0700

    test: Update test.php

--- test.php
+++ test.php
@@ -4,21 +4,20 @@ $config = [
     "url" => "file:database.db",
     // "authToken" => "secrettoken",
     // "syncUrl" => "libsql://database-org.turso.io",
-    // "syncInterval" => null,
-    // "encryptionKey" => null
+    // "syncInterval" => null
 ];
 
 $db = new LibSQL($config);
 echo $db->version() . PHP_EOL;
 
-// Execute the query
-$result = $db->query('SELECT * FROM users WHERE id = :id', [':id' => 1]);
+// // Execute the query
+// $result = $db->query('SELECT * FROM users');
 
-// Check if the query was successful and print the result
-if ($result !== false) {
-    var_dump($result);
-} else {
-    echo "Query failed.\n";
-}
+// // Check if the query was successful and print the result
+// if ($result !== false) {
+//     var_dump($result);
+// } else {
+//     echo "Query failed.\n";
+// }
 
-$db->close();
+// $db->close();
