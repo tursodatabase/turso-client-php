cca4ee75ce4c2fb8fde6053e53ec24484846549d
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-05-06 06:36:44 +0700

    feat(hooks): Add version.rs

---
+++ src/hooks/version.rs
@@ -0,0 +1,12 @@
+use crate::LIBSQL_PHP_VERSION;
+
+/// Retrieves the version of LibSQL.
+///
+/// # Returns
+///
+/// Returns a string representing the version of LibSQL.
+pub fn get_version() -> String {
+    let lisql_version =
+        "LibSQL Core Version ".to_string() + ": " + &libsql::version() + "-" + &libsql::version_number().to_string()  + " - " + "LibSQL PHP Extension Version: " + LIBSQL_PHP_VERSION;
+    lisql_version
+}
