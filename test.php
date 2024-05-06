b47e520d355a4409f3ea9c123a4c075595fd6e59
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-05-06 18:57:32 +0700

    chore: remote test content

--- test.php
+++ test.php
@@ -1,23 +1,3 @@
 <?php
 
-$config = [
-    "url" => "file:database.db",
-    // "authToken" => "secrettoken",
-    // "syncUrl" => "libsql://database-org.turso.io",
-    // "syncInterval" => null
-];
-
-$db = new LibSQL($config);
-echo $db->version() . PHP_EOL;
-
-// // Execute the query
-// $result = $db->query('SELECT * FROM users');
-
-// // Check if the query was successful and print the result
-// if ($result !== false) {
-//     var_dump($result);
-// } else {
-//     echo "Query failed.\n";
-// }
-
-// $db->close();
+// Noting here
