acfd74a78139c37c4000d0736f629bb6c0dcf422
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-05-23 14:53:45 +0700

    feat(sync): add sync method into stub

--- libsql_php_extension.stubs.php
+++ libsql_php_extension.stubs.php
@@ -496,5 +496,20 @@ namespace {
         public function close()
         {
         }
+
+        /**
+         * Sync the database.
+         * 
+         * # Example Usage
+         * 
+         * ```
+         * $db->sync();
+         * ```
+         *
+         * @return void The result of the sync operation.
+         */
+        public function sync()
+        {
+        }
     }
 }
