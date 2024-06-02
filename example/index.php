ea86cf17938f87516d02253caf4cbeb0d4045073
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-06-02 21:33:14 +0700

    docs: example usage

--- example/index.php
+++ example/index.php
@@ -5,18 +5,24 @@ use Doctrine\DBAL\DriverManager;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $params = [
-    "url"               => "database.db",
-    "auth_token"        => "",
-    "sync_url"          => "",
-    "sync_interval"     => 5,
-    "read_your_writes"  => true,
-    "encryption_key"    => "",
-    "remote_only"       => true,
+    "url"               => ":memory:",
     'driverClass'       => \Darkterminal\LibSQL\DBAL\Driver::class,
 ];
 
 $db = DriverManager::getConnection($params);
 
-$result = $db->executeQuery("SELECT name, userId, email FROM users LIMIT 2")->fetchAllAssociativeIndexed();
+$createTable = "CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER
+)";
+$db->executeStatement($createTable);
+
+$insertUsers = "INSERT INTO users (name, age) VALUES ('Budi Dalton', 49);
+INSERT INTO users (name, age) VALUES ('Sujiwo Tedjo', 50);";
+
+$db->getNativeConnection()->executeBatch($insertUsers);
+
+$result = $db->executeQuery("SELECT * FROM users")->fetchAllAssociative();
 var_dump($result);
 $db->close();
