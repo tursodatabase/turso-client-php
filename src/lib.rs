7897e46a732ffadcbaa98ac1539ec36fb171a8d9
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-03-02 16:37:27 +0700

    feat: create minor update and support PHP8.4

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ lazy_static::lazy_static! {
     static ref STATEMENT_REGISTRY: Mutex<HashMap<String, libsql::Statement>> = Mutex::new(HashMap::new());
 }
 
-pub const LIBSQL_PHP_VERSION: &str = "1.4.4";
+pub const LIBSQL_PHP_VERSION: &str = "1.5.0";
 
 /// Represents the flag for opening a database in read-only mode.
 pub const LIBSQL_OPEN_READONLY: i32 = 1;
@@ -462,7 +462,7 @@ pub extern "C" fn libsql_php_extension_info(_module: *mut ext_php_rs::zend::Modu
         ext_php_rs::ffi::php_info_print_table_row(
             2,
             "LibSQL PHP version\0".as_ptr() as *const i8,
-            "1.4.4\0".as_ptr() as *const i8,
+            "1.5.0\0".as_ptr() as *const i8,
         );
         ext_php_rs::ffi::php_info_print_table_row(
             2,
